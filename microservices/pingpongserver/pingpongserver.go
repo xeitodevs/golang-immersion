d7d521e9d45d161beaac8a447a79ab67f3a4958a
zucchinidev <azga84@gmail.com>
2018-09-05 16:42:31 +0200

    Use JSON to response and add POST http method handler

--- microservices/pingpongserver/pingpongserver.go
+++ microservices/pingpongserver/pingpongserver.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -16,6 +20,14 @@ const port = "3333"
 type server struct {
 	httpServer *http.Server
 	listener   net.Listener
+}
+
+type Response struct {
+	Msg string `json:"msg,omitempty"`
+}
+
+type Request struct {
+	Name string `json:"name"`
 }
 
 func (s *server) listenAndServe() error {
@@ -42,23 +54,48 @@ func (s *server) shutdown() error {
 }
 
 func newServer(port string) *server {
-	pongHandle := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Ping")
+	nameHandle := func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("error reading request body", err)
+			return
+		}
+
+		var request Request
+		err = json.Unmarshal(body, &request)
+		if err != nil {
+			log.Println("decode the incoming json fail", err)
+			return
+		}
+
+		log.Println("Request: ", request)
+		response := &Response{Msg: request.Name}
+		b, err := json.Marshal(response)
+		if err != nil {
+			log.Println("json marshal error", err)
+		}
+		stringResponse := string(b)
+		log.Println("Response: ", stringResponse)
+		io.WriteString(w, stringResponse)
 	}
 
-	pingHandle := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Pong")
+	status := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "ok")
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/pong", pongHandle)
-	mux.HandleFunc("/ping", pingHandle)
+	mux.HandleFunc("/name", nameHandle)
+	mux.HandleFunc("/status", status)
 
 	httpServer := &http.Server{Addr: ":" + port, Handler: mux}
 	return &server{httpServer: httpServer}
 }
 
 func main() {
+	var port string
+	flag.StringVar(&port, "port", "3333", "./pingpongserver -port 3333")
+	flag.Parse()
+
 	// a channel to receive unix signals
 	sigs := make(chan os.Signal, 1)
 	// a channel to receive a stop confirmation on interrupt
@@ -84,6 +121,7 @@ func main() {
 	}()
 	// Ctrl-C sends a SIGINT signal to the program
 	fmt.Println("Ctrl-C to interrupt...")
+	fmt.Println("POST: :PORT/name {name: \"Andrea\"}")
 	<-done
 	fmt.Println("Exiting...")
 }
